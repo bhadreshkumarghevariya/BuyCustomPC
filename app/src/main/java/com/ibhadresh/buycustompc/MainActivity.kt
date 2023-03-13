8857d91947a097c8cd3daad5dc862efd59b13fc2
bhadreshkumarghevariya <bhadreshkumarghevariya@gmail.com>
2023-03-13 02:06:32 -0400

    Initial commit

---
+++ app/src/main/java/com/ibhadresh/buycustompc/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.ibhadresh.buycustompc
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val btnBuildYourOwn = findViewById<Button>(R.id.btnBuildYourOwn)
+        btnBuildYourOwn.setOnClickListener {
+            val intent = Intent(this,MotherBoardSelection::class.java)
+            startActivity(intent)
+        }
+    }
+}
---
+++ app/src/main/java/com/ibhadresh/buycustompc/MotherBoardSelection.kt
@@ -0,0 +1,11 @@
+package com.ibhadresh.buycustompc
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MotherBoardSelection : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_mother_board_selection)
+    }
+}
