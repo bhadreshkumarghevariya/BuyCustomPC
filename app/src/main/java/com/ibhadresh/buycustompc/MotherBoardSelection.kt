3aaa5294a2ff56661d1969ebdf091dd5812ec3a8
bhadreshkumarghevariya <bhadreshkumarghevariya@gmail.com>
2023-03-13 04:27:10 -0400

    Created Mother Board Recycler View and showing all the mother boards from firebase.

--- app/src/main/java/com/ibhadresh/buycustompc/MotherBoardSelection.kt
+++ app/src/main/java/com/ibhadresh/buycustompc/MotherBoardSelection.kt
@@ -2,10 +2,44 @@ package com.ibhadresh.buycustompc
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.database.*
 
 class MotherBoardSelection : AppCompatActivity() {
+    private lateinit var dbref : DatabaseReference
+    private lateinit var motherBoardRecyclerView: RecyclerView
+    private lateinit var motherBoardArrayList: ArrayList<MotherBoard>
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_mother_board_selection)
+
+        motherBoardRecyclerView = findViewById(R.id.motherBoardRecyclerView)
+        motherBoardRecyclerView.layoutManager = LinearLayoutManager(this)
+        motherBoardRecyclerView.setHasFixedSize(true)
+
+        motherBoardArrayList = arrayListOf<MotherBoard>()
+        getMotherBoardData()
+    }
+
+    private fun getMotherBoardData() {
+        dbref = FirebaseDatabase.getInstance().getReference("MotherBoards")
+
+        dbref.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                if(snapshot.exists()){
+                    for(userSnapShot in snapshot.children){
+                        val motherBoard = userSnapShot.getValue(MotherBoard::class.java)
+                        motherBoardArrayList.add(motherBoard!!)
+                    }
+                    motherBoardRecyclerView.adapter = MotherBoardAdapter(motherBoardArrayList)
+                }
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                TODO("Not yet implemented")
+            }
+
+        })
     }
 }
