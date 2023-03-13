e4f53e8368088288214279a0f01276816f3dfb47
bhadreshkumarghevariya <bhadreshkumarghevariya@gmail.com>
2023-03-13 02:28:46 -0400

    Created Adapter class for MotherBoard

---
+++ app/src/main/java/com/ibhadresh/buycustompc/MotherBoardAdapter.kt
@@ -0,0 +1,35 @@
+package com.ibhadresh.buycustompc
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class MotherBoardAdapter(private val motherBoardList : ArrayList<MotherBoard>):RecyclerView.Adapter<MotherBoardAdapter.MotherBoardViewHolder>() {
+
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MotherBoardViewHolder {
+        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.mother_board_item,parent,false)
+        return MotherBoardViewHolder(itemView)
+    }
+
+    override fun getItemCount(): Int {
+        return motherBoardList.size
+    }
+
+    override fun onBindViewHolder(holder: MotherBoardViewHolder, position: Int) {
+        val currentMotherBoard = motherBoardList[position]
+
+        holder.productName.text = currentMotherBoard.productName
+        holder.socketType.text = currentMotherBoard.socketType
+        holder.description.text = currentMotherBoard.description
+    }
+
+    class MotherBoardViewHolder(itemView : View) : RecyclerView.ViewHolder(itemView){
+        val productName : TextView = itemView.findViewById(R.id.tvProductName)
+        val socketType : TextView = itemView.findViewById(R.id.tvSocketType)
+        val description : TextView =itemView.findViewById(R.id.tvDescription)
+    }
+}
